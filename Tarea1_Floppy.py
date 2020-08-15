60d679b0601c51a414ff692ae717c2cc5dbd3dd3
larissaOjeda <46902494+larissaOjeda@users.noreply.github.com>
2020-08-14 23:33:10 -0500

    Update Tarea1_Floppy.py

--- Tarea1_Floppy.py
+++ Tarea1_Floppy.py
@@ -3,7 +3,7 @@
 @author: larissa
 """
 
-#Ejerciccio 3 
+#Ejerciccio 3 (Este ta re bien según io c: ) 
 def interseccion(list1,list2):
         res = False
         for x in list1: 
@@ -14,7 +14,7 @@ def interseccion(list1,list2):
                 
 print(interseccion(["alana","kevin","rob","mike"], ["Georgia","Eva","Felix"]))
                     
-#Ejercicio 6
+#Ejercicio 6 (Si jala pero hay que hacer funciones pa que quede más bello c: )
 #Tal vez para este ejercicio convendría escribir la función multiplicacionValida y otra llamada multiplicacion
 #de manera que el resultado solo mande llamar a la funcion multiplicacion para que se vea mas modular el código 
 p = int (input("Intrdouce el número de renglones de la Matriz A: " ))
@@ -54,15 +54,19 @@ for i in range(p):
     print()
     
 
-#Ejercicio 8 (más o menos)
-palabras = []
-count = 0
+#Ejercicio 8 (más o menos falta revisar lo de leer el archivo bien c:  ) 
+def word_counter (filepath):
+    text_file = open("textFile.txt", "r") #Lee el archivo
+    data = text_file.read()
+    words = data.split()  #Separa un string en una lista donde cada palabra es un elemento de la lista 
+    counts = dict()  #crea un diccionario 
+    for word in words : #ciclo para recorrer cada palabra del texto 
+        if word in counts: 
+            counts[word] += 1 #Si la palabra ya se encontraba en counts entonces se suma una unidad a la cuenta 
+        else: 
+            counts[word] = 1  #Si es una palabra nueva entonces incializa su cuenta en 1 
+    return counts #regresa el diccionario con el número de veces que se repite cada palabra
 
-with open ("text.txt","r") as f:
-    # Toma una lista de lineas en el archivo y lo convierte en un set
-    palabras = set(f.readlines()) 
-    count = len(palabras) 
 
-print(count)
 
 
